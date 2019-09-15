dbda3e3459cc8bbf7fc7861dc3d26fddfdc49a04
Ashish Goyal <31183326+goyal007@users.noreply.github.com>
2019-09-15 16:31:25 +0530

    SLDPTA

---
+++ Lab2/a.py
@@ -0,0 +1,52 @@
+#Single Layer Discrete Perceptron Training Algorithm (Assign-2)
+
+def signum(input):
+    if input>0:
+        return 1
+    else:
+        return -1
+import numpy as np
+c=0.5 #learning rate
+w=[[0],[0],[0],[0]]
+x1=[[0],[0],[0],[1]]
+x2=[[0],[0],[1],[1]]
+x3=[[0],[1],[0],[1]]
+x4=[[0],[1],[1],[1]]
+x5=[[1],[0],[0],[1]]
+x6=[[1],[0],[1],[1]]
+x7=[[1],[1],[0],[1]]
+x8=[[1],[1],[1],[1]]
+
+d=[-1,1,-1,1,-1,1,-1,1]
+#print(type(w))
+w=np.array(w)
+x1=np.array(x1)
+x2=np.array(x2)
+x3=np.array(x3)
+x4=np.array(x4)
+x5=np.array(x5)
+x6=np.array(x6)
+x7=np.array(x7)
+x8=np.array(x8)
+#print(type(w))
+
+x=[x1,x2,x3,x4,x5,x6,x7,x8]
+for j in range(30):
+    print("epoch", j+1)
+    count=0
+    for i in range(8):
+        net1=(np.transpose(w)).dot(x[i])
+        #print(net1)
+        r=signum(net1)
+        r=d[i]-r
+        if(r!=0):
+            count=1
+        w= w + (c*r*x[i])/2
+        for weight in w:
+            print(weight[0],end="\t")
+        print('\n')
+    if(count==0):
+        print("stop")
+        break
+            
+
